49f531ae789dd97a3e724f79979cbcce75f12501
martine <Zhane_Nox@hotmail.ca>
2018-11-28 13:35:09 -0500

    fait du visiuel pour des pages

--- TestSiteWebUQO/resources/views/etucycletrois.blade.php
+++ TestSiteWebUQO/resources/views/etucycletrois.blade.php
@@ -1,9 +1,47 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
+    <h1>RENSEIGNEMENTS UTILES POUR LES ÉTUDIANTS DE 3e CYCLE</h1>
+    <ul>
 
+        <li><p>Formulaires</p></li>
+        <ul>
+            <li><p><a href="http://w4.uqo.ca/dii/pdf/LectureDirigee_inscription.pdf">INF9083 Lecture dirigée en sciences et technologies de l'information - Inscription</a></p></li>
+            <li><p><a href="http://w4.uqo.ca/dii/pdf/Atelier_inscription.pdf">INF9093, INF9103, INF9123, INF9133 - Inscription à un atelier</a></p></li>
+            <li><p>INF9006 Examen de synthèse</p></li>
+            <ul>
+                <li><p><a href="">Directives</a></p></li>                                                               <!–– To Do faire donwload onclik ––>
+                <li><p><a href="">Proposition des sujets</a></p></li>                                                   <!–– To Do faire donwload onclik ––>
+                <li><p><a href="http://w4.uqo.ca/dii/pdf/ExamenSynthese_inscription.pdf">Inscription</a></p></li>
+                <li><p><a href="">Évaluation</a></p></li>                                                               <!–– To Do faire donwload onclik ––>
+            </ul>
 
+            <li><p>INF9009 Projet de thèse</p></li>
+            <ul>
+                <li><p><a href="">Directives</a></p></li>                                                               <!–– To Do faire donwload onclik ––>
+                <li><p><a href="http://w4.uqo.ca/dii/pdf/ProjetThese_inscription.pdf">Inscription</a></p></li>
+                <li><p><a href="http://w4.uqo.ca/dii/pdf/ProjetThese_inscription.pdf">Évaluation</a></p></li>
+            </ul>
+            <li><p><a href="">Autres formulaires et procédures d'évaluation</a></p></li>
+        </ul>
 
+        <li><p>Bourses et aide financière</p></li>
+        <ul>
+            <li><p><a href="https://uqo.ca/etudiants/financer-vos-etudes/bourses-d-etudes">Bourses et aides financières (sur le site de l'UQO)</a></p></li>
+            <li><p><a href="http://w4.uqo.ca/pelc/caldi/francais/bourses.htm">Bourse en calcul distribué (CALDI)</a></p></li>
+        </ul>
+
+    </ul>
 
 @endsection
+
+</body>
+</html>
