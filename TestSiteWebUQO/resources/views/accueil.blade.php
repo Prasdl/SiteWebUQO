01b77b0c0c1b5c596437702370d8acf44eceb419
martine <zhane_nox@hotmail.ca>
2018-11-28 11:38:32 -0500

    mit du visiuel pour la page aceuil

--- TestSiteWebUQO/resources/views/accueil.blade.php
+++ TestSiteWebUQO/resources/views/accueil.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
+    <h1>ÉVÈNEMENTS</h1>
 
+    <?php
 
+    /*
+    To Do faire le code pour afficher les évènements ????
+    */
+
+    ?>
 
 
 @endsection
+
+</body>
+</html>
