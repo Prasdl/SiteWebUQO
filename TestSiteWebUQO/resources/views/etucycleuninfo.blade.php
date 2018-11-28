21f5ea90d129b63c32d6648012852f7adf4faaa8
martine <zhane_nox@hotmail.ca>
2018-11-28 13:47:25 -0500

    fait du visiuel pour des pages

--- TestSiteWebUQO/resources/views/etucycleuninfo.blade.php
+++ TestSiteWebUQO/resources/views/etucycleuninfo.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
-
-
-
+    <h1>BIENVENUE AU MODULE DE L'INFORMATIQUE</h1>
+    <ul>
+        <li><p><a href="">Équivalence des cours</a></p></li>                                                            <!–– To Do faire les page lier et les liens ––>
+        <li><p><a href="http://etudier.uqo.ca/horaire">Horaire des cours (heure, salle)</a></p></li>
+        <li><p><a href="http://w4.uqo.ca/dii/module/TIC/TIC.pdf">Pourquoi choisir d'étudier l'informatique à l'UQO ?</a></p></li>      <!–– pdf du site ??? ––>
+    </ul>
 
 @endsection
+
+
+</body>
+</html>
