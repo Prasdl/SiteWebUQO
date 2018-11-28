0d3eee6c4698cb2d55b7d681cc2ebfd942efbcdb
martine <zhane_nox@hotmail.ca>
2018-11-28 11:48:02 -0500

    mit du visiuel pour des page

--- TestSiteWebUQO/resources/views/ceeremploi.blade.php
+++ TestSiteWebUQO/resources/views/ceeremploi.blade.php
@@ -1,9 +1,26 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
+    <h1>CRÉER UNE OFFRE D'EMPLOI</h1>
 
+    <?php
 
+    /*
+    To Do faire le form
+    */
+
+    ?>
 
 
 @endsection
+
+</body>
+</html>
--- TestSiteWebUQO/resources/views/cycledeuxtrois.blade.php
+++ TestSiteWebUQO/resources/views/cycledeuxtrois.blade.php
@@ -1,9 +1,29 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
+    <h1>PROGRAMMES D'ÉTUDES DE 2e ET 3e CYCLES</h1>
 
+    <p><a href="https://apps.uqo.ca/AdmissionEnLigne/Directives.aspx">Demande d'admission en ligne</a></p>
+    <p>Programmes de 2e cycle</p>
+    <p><a href="">Maîtrise en sciences et technologies de l'information (profil mémoire)</a></p>
+    <p><a href="">Maîtrise en sciences et technologies de l'information (profil professionnel)</a></p>
+    <p><a href="">Diplôme d'études supérieures spécialisées en sciences et technologies de l'information</a></p>
+    <p><a href="">Programme court de deuxième cycle en télécommunications et sécurité</a></p>
+    <p><a href="">Programme court de deuxième cycle en traitement des données</a></p>
 
+    <p>Programmes de 3e cycle</p>
+    <p><a href="">Doctorat en sciences et technologies de l'information</a></p>
 
 
 @endsection
+
+</body>
+</html>
