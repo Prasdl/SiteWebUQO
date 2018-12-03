f008bb1402e1682b6635afab04d7674e99a9cca9
Martine <zhane_nox@hotmail.ca>
2018-12-03 12:19:30 -0500

    Visuel pour les pages 1cycle et plan du site

--- TestSiteWebUQO/resources/views/plansite.blade.php
+++ TestSiteWebUQO/resources/views/plansite.blade.php
@@ -1,9 +1,106 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
+    <h1>PLAN DU SITE</h1>
+    <ul>
+        <!–– To Do faire lien vers site ––>
+
+        <p>Programmes d'études</p>
+        <ul>
+            <li><p><a href="">Programmes d'études de 1er cycle</a></p></li>
+            <li><p><a href="">Programmes d'études de 2e et 3e cycles</a></p></li>
+            <li><p><a href="">Plans de cours</a></p></li>
+
+            <ul>
+                <li><p>Plans de cours par programmes</p></li>
+                <ul>
+                    <li><p><a href="">7643 Bacc. en génie informatique</a></p></li>
+                    <li><p><a href="">7833 Bacc. en informatique</a></p></li>
+                    <li><p><a href="">4108 Certificat en informatique de gestion</a></p></li>
+                    <li><p><a href="">4802 Certificat en technologies de l'information</a></p></li>
+                    <li><p><a href="">3781 Maîtrise en informatique</a></p></li>
+                    <li><p><a href="">3081 Doctorat en sciences et technologies de l'information</a></p></li>
+                    <li><p><a href="">0000 Autres cours</a></p></li>
+                </ul>
+                <li><p>Plans de cours par session</p></li>
+                <ul>
+                    <li><p><a href="">Hiver 2007</a></p></li>
+                    <li><p><a href="">Automne 2006</a></p></li>
+                    <li><p><a href="">Été 2006</a></p></li>
+                    <li><p><a href="">Hiver 2006</a></p></li>
+                    <li><p><a href="">Automne 2005</a></p></li>
+                    <li><p><a href="">Été 2005</a></p></li>
+                </ul>
+            </ul>
+
+            <li><p><a href="">Module de l'informatique</a></p></li>
+            <li><p><a href="">Module de l'ingénierie</a></p></li>
+        </ul>
+    </ul>
+
+    <p>Étudiants de 1er cycle</p>
+    <ul>
+        <li><p><a href="">Une séance d'exercices? Un travail dirigé? Un devoir?</a></p></li>
+        <li><p>Horaire des activités</p></li>
+        <ul>
+            <ul>
+                <li><p><a href="">Informatique - Automne 2006</a></p></li>
+                <li><p><a href="">Informatique - Hiver 2007</a></p></li>
+                <li><p><a href="">Génie Informatique - Automne 2006</a></p></li>
+                <li><p><a href="">Génie Informatique - Hiver 2007</a></p></li>
+            </ul>
+            <li><p><a href="">Séances de travaux pratiques, séances de travaux dirigés, séances d'exercices</a></p></li>
+            <li><p><a href="">Rapport de laboratoire</a></p></li>
+            <li><p><a href="">Plans des locaux de laboratoires de génie</a></p></li>
+            <li><p><a href="">Manuels</a></p></li>
+            <li><p><a href="">Listes des logiciels installés par local</a></p></li>
+            <li><p><a href="">Règles de sécurité aux laboratoires de génie</a></p></li>
+            <li><p><a href="">Associations professionnelles</a></p></li>
+    </ul>
+    </ul>
 
 
+    <p>Étudiants de 2e et 3e cycles</p>
+    <ul>
+        <li><p><a href="">Formulaires (maîtrise)</a></p></li>
+        <li><p><a href="">Formulaires (doctorat)</a></p></li>
+        <li><p><a href="">Nos étudiants de maîtrise</a></p></li>
+        <li><p><a href="">Diplômés du programme de maîtrise</a></p></li>
+    </ul>
+
+    <p>Recherche</p>
+    <ul>
+        <li><p><a href="">Domaines de spécialisation des professeurs</a></p></li>
+        <li><p><a href="">Équipes de recherche</a></p></li>
+        <li><p><a href="">Mémoires de maîtrise</a></p></li>
+        <li><p><a href="">Rapports de recherche</a></p></li>
+        <li><p><a href="">Projets de recherche sur la sécurité</a></p></li>
+    </ul>
+
+    <p>Personnel</p>
+    <ul>
+        <li><p><a href="">Professeurs</a></p></li>
+        <li><p><a href="">Personnel de soutien</a></p></li>
+    </ul>
+
+    <p>Informations générales</p>
+    <ul>
+        <li><p><a href="">Partenaires éducatifs</a></p></li>
+        <li><p><a href="">Nouvelles</a></p></li>
+        <li><p><a href="">Galerie de photos</a></p></li>
+        <li><p><a href="">Pour nous joindre</a></p></li>
+    </ul>
 
 
 @endsection
+
+</body>
+</html>
--- TestSiteWebUQO/resources/views/premiercycle.blade.php
+++ TestSiteWebUQO/resources/views/premiercycle.blade.php
@@ -1,9 +1,45 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
 
+    <h1>PROGRAMMES D'ÉTUDES DE 1er CYCLE</h1>
 
+    <li>Programmes de baccalauréat</li>
+    <ul>
+        <li><p><a href="http://etudier.uqo.ca/programmes/7833">Baccalauréat en informatique</a></p></li>
+        <li><p><a href="http://etudier.uqo.ca/programmes/7033">Baccalauréat en informatique (cheminement coopératif)</a></p></li>
+        <li><p><a href="http://etudier.uqo.ca/programmes/7643">Baccalauréat en génie informatique</a></p></li>
+        <ul>
+            <li><p>Concentration Systèmes informatiques</p></li>
+            <ul>
+                <li><p>Option générale</p></li>
+                <li><p>Option Sécurité des systèmes d'information</p></li>
+            </ul>
+        </ul>
+        <li><p><a href="http://etudier.uqo.ca/programmes/7543">Baccalauréat en génie électrique</a></p></li>
+    </ul>
 
+    <li>Programmes de certificat</li>
+    <ul>
+        <li><p><a href="http://etudier.uqo.ca/programmes/4802">Certificat en technologies de l'information</a></p></li>
+        <li><p><a href="http://etudier.uqo.ca/programmes/4108">Certificat en informatique de gestion</a></p></li>
+    </ul>
+    <li>Programmes courts</li>
+    <ul>
+        <li><p><a href="http://etudier.uqo.ca/programmes/0542">Programme court de premier cycle en gestion des technologies de l'information</a></p></li>
+
+    </ul>
+    <li><a href="https://apps.uqo.ca/AdmissionEnLigne/Directives.aspx">Demande d'admission en ligne</a></li>
 
 @endsection
+
+</body>
+</html>
