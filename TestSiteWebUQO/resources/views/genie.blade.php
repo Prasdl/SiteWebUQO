3089472a502ee11e5a8bf8cb641f8e640090e87e
martine <zhane_nox@hotmail.ca>
2018-12-03 11:44:04 -0500

    Visuel pour les pages infomartique et génie

--- TestSiteWebUQO/resources/views/genie.blade.php
+++ TestSiteWebUQO/resources/views/genie.blade.php
@@ -1,9 +1,43 @@
 @extends('layouts.template')
 
+        <!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
 
+    <h2>Programmes de génie offerts à l'UQO</h2>
 
+    <h2>Baccalauréat en génie électrique  <a href="http://services.uqo.ca/GuideEtudesWeb/programmes/7543.html">description</a> <a href="https://uqo.ca/etudiants/admission">admission</a></h2>
+    <p>Les étudiants aborderont l'étude du domaine de l'énergie électrique sous l'optique de l'efficacité énergétique et des énergies renouvelables.</p>
 
+    <h2>Baccalauréat en génie informatique <a href="http://services.uqo.ca/GuideEtudesWeb/programmes/7643.html">description</a> <a href="https://uqo.ca/etudiants/admission">admission</a></h2>
+    <p>Les étudiants aborderont l'étude du domaine de l'énergie électrique sous l'optique de l'efficacité énergétique et des énergies renouvelables.</p>
+
+    <h2><a href="https://uqo.ca/etudiants/aide-financiere">Financer ses études</a></h2>
+    <p>Bureau d’aide financière du Bureau du registraire et des services aux étudiants</p>
+    <p>Téléphone : 819 773-1680</p>
+    <p>Pavillon Lucien-Brault, bureau B-0170</p>
+
+    <h2><a href="https://uqo.ca/etudiants/bourses-detudes">Bourses d’études</a></h2>
+    <p>Paul Courtemanche</p>
+    <p>Responsable des programmes de bourses à l'UQO</p>
+    <p>Téléphone : 819 595-3900, poste 1684</p>
+    <p>Pavillon Lucien-Brault, bureau B-0170</p>
+    <p>paul.courtemanche@uqo.ca</p>  <!–– To Do faire mailto ––>
+
+    <h2><a href="https://uqo.ca/stages">Formation pratique intégrée en génie</a></h2>
+    <p>Geneviève Biron</p>
+    <p>Coordonnatrice de stage - programmes coopératifs</p>
+    <p>Téléphone : 819 595-3900, poste 1762</p>
+    <p>Pavillon Alexandre-Taché, bureau F-0242</p>
+    <p>genevieve.biron@uqo.ca</p>  <!–– To Do faire mailto ––>
 
 @endsection
+
+</body>
+</html>
--- TestSiteWebUQO/resources/views/informatique.blade.php
+++ TestSiteWebUQO/resources/views/informatique.blade.php
@@ -1,9 +1,79 @@
 @extends('layouts.template')
 
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
 @section('template')
 
+    <h1>L'INFORMATIQUE À L'UQO</h1>
+
+    <h2>Programmes</h2>
+
+    <p>Le Module de l'informatique offre quatre programmes de premier cycle en informatique soit le :</p>
+    <ul>
+        <li><p>Baccalauréat en informatique</p></li>
+        <li><p>Certificat en informatique de gestion</p></li>
+        <li><p>Certificat en technologies de l'information</p></li>
+        <li><p>Programme court en gestion des technologies de l'information</p></li>
+    </ul>
+
+    <h2>Qu'est-ce que l'informatique ?</h2>
+
+    <p>L'informatique a pour objet la théorie, la conception et le développement des systèmes informatiques et des logiciels.
+        Elle comprend les mathématiques liées à l'informatique, la méthodologie (langages et systèmes) de programmation,
+        les architectures logicielles et de systèmes, ainsi qu'une vaste gamme de domaines d'application.</p>
+
+    <h2>Emplois pour l'étudiant titulaire d'un diplôme en informatique</h2>
+    <p>De façon générale, le travail de l'informaticien consiste à structurer et à représenter l'information,
+        et à en faciliter l'utilisation et la transmission. À travers sa formation académique, l'étudiant développera en particulier les connaissances, capacités et qualités suivantes :</p>
+    <ul>
+        <li><p>comprendre et appliquer le processus d'ingénierie du logiciel;</p></li>
+        <li><p>structurer, planifier et gérer des projets informatiques;</p></li>
+        <li><p>développer des logiciels de qualité tout en respectant les budgets et les échéances;</p></li>
+        <li><p>mettre en exploitation et exploiter des systèmes informatisés;</p></li>
+        <li><p>s'adapter à l'évolution rapide des technologies de l'information;</p></li>
+        <li><p>travailler efficacement en équipe multidisciplinaire tout en respectant les règles de la profession;</p></li>
+        <li><p>communiquer efficacement tant à l'oral qu'à l'écrit;</p></li>
+        <li><p>comprendre, analyser et définir la part des technologies de l'information dans un processus scientifique ou technique;</p></li>
+        <li><p>sélectionner et intégrer à des processus scientifiques ou techniques des solutions existantes issues de technologies de l'information;</p></li>
+        <li><p>spécifier, concevoir et mettre en oeuvre des nouvelles solutions mariant les technologies de l'information aux disciplines scientifiques et techniques.</p></li>
+    </ul>
+
+    <p>Les postes en informatique peuvent être aussi variés que</p>
+    <ul>
+        <li><p>Administrateur de bases de données</p></li>
+        <li><p>Analyste en sécurité</p></li>
+        <li><p>Analyste informatique</p></li>
+        <li><p>Analyste-programmeur</p></li>
+        <li><p>Architecte technologique</p></li>
+        <li><p>Chef d'équipe</p></li>
+        <li><p>Chercheur</p></li>
+        <li><p>Concepteur informatique</p></li>
+        <li><p>Directeur de projet</p></li>
+        <li><p>Directeur de service</p></li>
+        <li><p>Professeur</p></li>
+    </ul>
 
 
+    <h2>Secteurs d'emplois en informatique durant les dernières années</h2>
+    <p>Plusieurs secteurs des technologies de l'information (TI) sont en pleine croissance, tel le commerce électronique, la sécurité, les méga-données, etc.</p>
+
+    <p>  De plus, des dizaines de milliers d'emplois seront disponibles au Canada dans les prochaines années que
+        ce soit aux gouvernements, dans de grandes ou de moyennes entreprises, dans de sociétés de consultants et/ou de fournisseurs de services informatiques.</p>
+
+    <h2>Marché de l'emploi</h2>
+    <ul>
+        <li><p><a href="https://www.guichetemplois.gc.ca/pieces_jointes-fra.do?cid=10536&lang=fra">Emploi-Avenir Québec</a></p></li>
+        <li><p><a href="https://www.technocompetences.qc.ca/">TechnoCompétences</a></p></li>
+
+    </ul>
 
 
 @endsection
+
+</body>
+</html>
