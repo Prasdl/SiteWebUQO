cd8f65ba3b1a39201df00b8eaa4827539bb0c80d
Martine <zhane_nox@hotmail.ca>
2018-12-03 15:39:14 -0500

    Fait des des routes accueil,genie,informatique,partenaires,plansite

--- TestSiteWebUQO/routes/web.php
+++ TestSiteWebUQO/routes/web.php
@@ -11,8 +11,38 @@
 |
 */
 
+
+/*
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::post('/contact/submit', 'MessagesController@submit');
+
+*/
+
+Route::get('/', function () {
+    return view('accueil');
+});
+
+
+Route::get('/plansite', function () {
+    return view('plansite');
+});
+
+
+Route::get('/genie', function () {
+    return view('genie');
+});
+
+Route::get('/informatique', function () {
+    return view('informatique');
+});
+
+Route::get('/', function () {
+    return view('accueil');
+});
+
+Route::get('/partenaire', function () {
+    return view('partenaires');
+});
