2b2ab7af0ba46d42b38a4310f897b0587f4b27df
Martine <zhane_nox@hotmail.ca>
2018-12-17 14:29:01 -0500

    Commencer les page personnel et professeure

--- TestSiteWebUQO/resources/views/personnel/personnel.blade.php
+++ TestSiteWebUQO/resources/views/personnel/personnel.blade.php
@@ -3,7 +3,97 @@
 @section('template')
 
 
-
+    <div id="personnel">
+        <h2>Une équipe à votre service</h2>
+        <div>
+            <div class="entrypersonnel">
+                <div class="picture">
+                    <img alt="Claudine Morais" style="border:1px solid" width="143" height="143" src="/Image/ClaudineMorais.png">
+                </div>
+                <div class="infos">
+<span class="textbold">Claudine&nbsp;Morais<br>
+</span>
+                    <b>Bur. : </b>
+                    <span class="textsmall">B-0122</span>
+                    <br>
+                    <b>Tél. : </b>
+                    <span class="textsmall">(819) 773-1620</span>
+                    <br>
+                    <span class="textvsmall">claudine.morais@uqo.ca</span>
+                </div>
+                <div class="taches">Modules de l'informatique et de l'ingénierie.
+                    Agente de soutien administratif à la gestion des études</div>
+            </div>
+            <div class="entrypersonnel">
+                <div class="picture">
+                    <img alt="Josée Charlebois" style="border:1px solid" width="143" height="143" src="">
+                </div>
+                <div class="infos">
+<span class="textbold">Josée&nbsp;Charlebois<br>
+</span>
+                    <b>Bur. : </b>
+                    <span class="textsmall">B-2040</span>
+                    <br>
+                    <b>Tél. : </b>
+                    <span class="textsmall">(819) 773-1600</span>
+                    <br>
+                    <span class="textvsmall">josee.charlebois@uqo.ca</span>
+                </div>
+                <div class="taches">Secrétaire de direction</div>
+            </div>
+            <div class="entrypersonnel">
+                <div class="picture">
+                    <img alt="Sharon Beaulne" style="border:1px solid" width="143" height="143" src="">
+                </div>
+                <div class="infos">
+<span class="textbold">Sharon&nbsp;Beaulne<br>
+</span>
+                    <b>Bur. : </b>
+                    <span class="textsmall">B-2040</span>
+                    <br>
+                    <b>Tél. : </b>
+                    <span class="textsmall">1614</span>
+                    <br>
+                    <span class="textvsmall">sharon.beaulne@uqo.ca</span>
+                </div>
+                <div class="taches">Technicienne en administration - gestion départementale</div>
+            </div>
+            <div class="entrypersonnel">
+                <div class="picture">
+                    <img alt="Antoine Shaneen" style="border:1px solid" width="143" height="143" src="/Image/AntoineShaneen.png">
+                </div>
+                <div class="infos">
+<span class="textbold">Antoine&nbsp;Shaneen<br>
+</span>
+                    <b>Bur. : </b>
+                    <span class="textsmall">B-0180</span>
+                    <br>
+                    <b>Tél. : </b>
+                    <span class="textsmall">1863</span>
+                    <br>
+                    <span class="textvsmall">antoine.shaneen@uqo.ca</span>
+                </div>
+                <div class="taches">Responsable des laboratoires de génie informatique</div>
+            </div>
+            <div class="entrypersonnel">
+                <div class="picture">
+                    <img alt="Abdelkrim Chebihi" style="border:1px solid" width="143" height="143" src="/Image/AbdelkrimChebihi.png">
+                </div>
+                <div class="infos">
+<span class="textbold">Abdelkrim&nbsp;Chebihi<br>
+</span>
+                    <b>Bur. : </b>
+                    <span class="textsmall">A-0302A</span>
+                    <br>
+                    <b>Tél. : </b>
+                    <span class="textsmall">1603</span>
+                    <br>
+                    <span class="textvsmall">abdelkrim.chebihi@uqo.ca</span>
+                </div>
+                <div class="taches">Technicien de laboratoire</div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
--- TestSiteWebUQO/resources/views/personnel/professeurs.blade.php
+++ TestSiteWebUQO/resources/views/personnel/professeurs.blade.php
@@ -2,6 +2,373 @@
 
 @section('template')
 
+    <div id="contenu">
+        <p class="text" id="cliquer">Cliquer un nom ou une photo pour avoir plus d'information sur le professeur.<br>
+        </p>
+        <div id="professeurs">
+            <div class="entry">
+                <div class="picture">
+                    <a href="dyn/profs/omar.abdulwahab.php">
+                        <img alt="Omar Abdul Wahab" style="border:1px solid" width="143" height="143" src="/Image/omar.abdulwahab.jpg">
+                    </a>
+                </div>
+                <div class="status2">&nbsp;</div>
+                <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/omar.abdulwahab.php">Omar&nbsp;Abdul&nbsp;Wahab</a>
+<br>
+</span>
+                    <b>Bur. : </b>B-2024<br>
+                    <b>Tél. : </b> <br>
+                    <span class="textvsmall"> </span>
+                </div>
+            </div>
+            <div id="professeurs">
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/kamel.adi.php">
+                            <img alt="Kamel Adi" style="border:1px solid" width="143" height="143" src="/Image/kamel.adi.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/kamel.adi.php">Kamel&nbsp;Adi</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2012<br>
+                        <b>Tél. : </b>1813<br>
+                        <span class="textvsmall">kamel.adi@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/mohand.allili.php">
+                            <img alt="Mohand S. Allili" style="border:1px solid" width="143" height="143" src="/Image/mohand.allili.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/mohand.allili.php">Mohand S.&nbsp;Allili</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2022<br>
+                        <b>Tél. : </b>1601<br>
+                        <span class="textvsmall">mohandsaid.allili@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/nadia.baaziz.php">
+                            <img alt="Nadia Baaziz" style="border:1px solid" width="143" height="143" src="/Image/nadia.baaziz.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/nadia.baaziz.php">Nadia&nbsp;Baaziz</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2041<br>
+                        <b>Tél. : </b>1600<br>
+                        <span class="textvsmall">nadia.baaziz@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/ilham.benyahia.php">
+                            <img alt="Ilham Benyahia" style="border:1px solid" width="143" height="143" src="/Image/ilham.benyahia.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/ilham.benyahia.php">Ilham&nbsp;Benyahia</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2034<br>
+                        <b>Tél. : </b>1749<br>
+                        <span class="textvsmall">ilham.benyahia@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/wojtek.bock.php">
+                            <img alt="Wojtek J. Bock" style="border:1px solid" width="143" height="143" src="/Image/wojtek.bock.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/wojtek.bock.php">Wojtek J.&nbsp;Bock</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2016<br>
+                        <b>Tél. : </b>1623<br>
+                        <span class="textvsmall">wojtek.bock@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/alain.charbonneau.php">
+                            <img alt="Alain Charbonneau" style="border:1px solid" width="143" height="143" src="/Image/alain.charbonneau.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/alain.charbonneau.php">Alain&nbsp;Charbonneau</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2026<br>
+                        <b>Tél. : </b>1704<br>
+                        <span class="textvsmall">alain.charbonneau@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/jurek.czyzowicz.php">
+                            <img alt="Jurek Czyzowicz" style="border:1px solid" width="143" height="143" src="/Image/jurek.czyzowicz.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/jurek.czyzowicz.php">Jurek&nbsp;Czyzowicz</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2020<br>
+                        <b>Tél. : </b>1618<br>
+                        <span class="textvsmall">jurek@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/alan.davoust.php">
+                            <img alt="Alan Davoust" style="border:1px solid" width="143" height="143" src="/Image/alan.davoust.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/alan.davoust.php">Alan&nbsp;Davoust</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2073<br>
+                        <b>Tél. : </b> <br>
+                        <span class="textvsmall"> </span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/tinko.eftimov.php">
+                            <img alt="Tinko Eftimov" style="border:1px solid" width="143" height="143" src="/Image/tinko.eftimov.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/tinko.eftimov.php">Tinko&nbsp;Eftimov</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2018<br>
+                        <b>Tél. : </b>1734<br>
+                        <span class="textvsmall">tinko.eftimov@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/karim.elguemhioui.php">
+                            <img alt="Karim El Guemhioui" style="border:1px solid" width="143" height="143" src="/Image/karim.elguemhioui.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/karim.elguemhioui.php">Karim&nbsp;El Guemhioui</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-0123<br>
+                        <b>Tél. : </b>1620<br>
+                        <span class="textvsmall">karim.elguemhioui@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/michal.iglewski.php">
+                            <img alt="Michal Iglewski" style="border:1px solid" width="143" height="143" src="/Image/michal.iglewski.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/michal.iglewski.php">Michal&nbsp;Iglewski</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2024<br>
+                        <b>Tél. : </b>1602<br>
+                        <span class="textvsmall">iglewski@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/michael.korwin-pawlowski.php">
+                            <img alt="Michael Korwin-Pawlowski" style="border:1px solid" width="143" height="143" src="/Image/michael.korwin-pawlowski.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/michael.korwin-pawlowski.php">Michael&nbsp;Korwin-Pawlowski</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2008<br>
+                        <b>Tél. : </b>1626<br>
+                        <span class="textvsmall">michael.korwin-pawlowski@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/ahmed.lakhssassi.php">
+                            <img alt="Ahmed Lakhssassi" style="border:1px solid" width="143" height="143" src="/Image/ahmed.lakhssassi.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/ahmed.lakhssassi.php">Ahmed&nbsp;Lakhssassi</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2014<br>
+                        <b>Tél. : </b>1610<br>
+                        <span class="textvsmall">ahmed.lakhssassi@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/frederic.lesage.php">
+                            <img alt="Frédéric Lesage" style="border:1px solid" width="143" height="143" src="/Image/frederic.lesage.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/frederic.lesage.php">Frédéric&nbsp;Lesage</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-0125<br>
+                        <b>Tél. : </b>1620<br>
+                        <span class="textvsmall">frederic.lesage@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/luigi.logrippo.php">
+                            <img alt="Luigi Logrippo" style="border:1px solid" width="143" height="143" src="/Image/luigi.logrippo.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/luigi.logrippo.php">Luigi&nbsp;Logrippo</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2010<br>
+                        <b>Tél. : </b>1885<br>
+                        <span class="textvsmall">luigi.logrippo@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/rokia.missaoui.php">
+                            <img alt="Rokia Missaoui" style="border:1px solid" width="143" height="143" src="/Image/rokia.missaoui.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/rokia.missaoui.php">Rokia&nbsp;Missaoui</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2032<br>
+                        <b>Tél. : </b>1709<br>
+                        <span class="textvsmall">rokia.missaoui@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/andrzej.pelc.php">
+                            <img alt="Andrzej Pelc" style="border:1px solid" width="143" height="143" src="/Image/andrzej.pelc.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/andrzej.pelc.php">Andrzej&nbsp;Pelc</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2028<br>
+                        <b>Tél. : </b>1625<br>
+                        <span class="textvsmall">andrzej.pelc@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/shamsodin.taheri.php">
+                            <img alt="Shamsodin Taheri" style="border:1px solid" width="143" height="143" src="/Image/shamsodin.taheri.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/shamsodin.taheri.php">Shamsodin&nbsp;Taheri</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2006<br>
+                        <b>Tél. : </b>1608<br>
+                        <span class="textvsmall">shamsodin.taheri@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/larbi.talbi.php">
+                            <img alt="Larbi Talbi" style="border:1px solid" width="143" height="143" src="/Image/larbi.talbi.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/larbi.talbi.php">Larbi&nbsp;Talbi</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2036<br>
+                        <b>Tél. : </b>1721<br>
+                        <span class="textvsmall">larbi.talbi@uqo.ca</span>
+                    </div>
+                </div>
+                <div class="entry">
+                    <div class="picture">
+                        <a href="dyn/profs/marek.zaremba.php">
+                            <img alt="Marek Zaremba" style="border:1px solid" width="143" height="143" src="/Image/marek.zaremba.jpg">
+                        </a>
+                    </div>
+                    <div class="status2">&nbsp;</div>
+                    <div class="textsmall">
+<span class="textbold">
+<a href="dyn/profs/marek.zaremba.php">Marek&nbsp;Zaremba</a>
+<br>
+</span>
+                        <b>Bur. : </b>B-2030<br>
+                        <b>Tél. : </b>1616<br>
+                        <span class="textvsmall">marek.zaremba@uqo.ca</span>
+                    </div>
+                </div>
+            </div>
+            <p>&nbsp;</p>
+        </div>
+    </div>
+
 
 
 
