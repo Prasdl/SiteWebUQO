4c1f79737ff79853d58ba20a060f190094f2bd24
Martine <zhane_nox@hotmail.ca>
2018-12-21 09:55:57 -0500

    Le carousel est fait

--- SiteWebUQO/resources/views/accueil.blade.php
+++ SiteWebUQO/resources/views/accueil.blade.php
@@ -5,28 +5,23 @@
 
     <!-- Test, y'a de quoi qui ne marche pas avec le background image <style>body{background-image: {{ asset('img/UniversiteBackground.jpg') }}}</style> -->
 
-    ***Ajouter un carousel ici***
 
-    <div>
-        <img src="/img/UniversiteBackground.jpg">
-    </div>
 
     <div class="container">
         <h2>Bienvenu</h2>
         <div  class="carousel slide" data-ride="carousel">
 
-            <!-- Wrapper for slides -->
+            <!-- Les element du carousel -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <img src="/img/UniversiteBackground.jpg"  style="width:100%;">
+                    <img src="/img/UniversiteBackground.jpg" style="width:100%;">
+                </div>
+                <div class="item">
+                    <img src="/img/infoLogo2.png"  style="width:100%;">
                 </div>
 
                 <div class="item">
-                    <img src="infoLogo2.png"  style="width:100%;">
-                </div>
-
-                <div class="item">
-                    <img src="infoLogo1.png"  style="width:100%;">
+                    <img src="/img/infoLogo.png"  style="width:100%;">
                 </div>
             </div>
 
